8e03185c6a85bcb4f2c9e5cad4b4d37d25651e82
MeetPatel-2005 <pmalay694@gmail.com>
2026-01-20 00:09:46 +0530

    lenis fixes

--- components/smooth-scroll/smooth-scroll.tsx
+++ components/smooth-scroll/smooth-scroll.tsx
@@ -2,30 +2,33 @@
 
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { ReactLenis, ReactLenisRef } from "lenis/react";
+import { ReactLenis } from "lenis/react";
+import type Lenis from "lenis";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const SmoothScroll = ({ children }: Props) => {
-  type NewType = ReactLenisRef;
-  const lenisRef = useRef<NewType | null>(null);
+export default function SmoothScroll({ children }: Props) {
+  const lenisRef = useRef<Lenis | null>(null);
 
   useEffect(() => {
-    const update = (time: number) => {
-      lenisRef.current?.lenis?.raf(time * 1000);
-    };
+    function update(time: number) {
+      lenisRef.current?.raf(time * 1000);
+    }
 
     gsap.ticker.add(update);
     return () => gsap.ticker.remove(update);
   }, []);
 
   return (
-    <ReactLenis root ref={lenisRef} options={{ autoRaf: false }}>
+    <ReactLenis
+      ref={(instance) => {
+        lenisRef.current = instance?.lenis ?? null;
+      }}
+      root
+    >
       {children}
     </ReactLenis>
   );
-};
-
-export default SmoothScroll;
+}
